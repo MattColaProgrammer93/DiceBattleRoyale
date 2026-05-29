3215c386e4eb289b3c35ed776828fc07750718a3
MattColaProgrammer93 <mattchoque115@gmail.com>
2026-05-28 20:21:58 -0700

    Creating the first scanner for validation and adding
    I have created the layout for the scanner that will validate if the
    player's name is a actual name and not just numbers. It will also check
    if there are any duplicate names in the playerList.

--- DiceBattleRoyaleProject/src/Game.java
+++ DiceBattleRoyaleProject/src/Game.java
@@ -1,6 +1,28 @@
+import java.util.*;
 
 public class Game {
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		Queue<Map<String, PlayerStats>> playerList = new LinkedList<>();
+		Scanner sc = new Scanner(System.in);
+		String input;
+		
+		// Setting the players
+		while (input != "quit") {
+			System.out.println("Enter player name");
+			String playerName = sc.nextLine();
+			boolean isAName = false;
+			if(!isAName) {
+				System.out.println("This isn't a valid name, try again.");
+			} else {
+				Map<String, PlayerStats> element = new HashMap<>();
+				boolean checkDuplicates;
+				if(!checkDuplicates) {
+					element.put(playerName, new PlayerStats());
+					playerList.add(element);
+				} else {
+					System.out.println("Player name is already on the list, try again.");
+				}
+			}
+		}
 	}
 }
