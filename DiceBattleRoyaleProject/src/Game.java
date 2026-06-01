0212ade0d3942645797f6bf4408b7c8d0360a596
MattColaProgrammer93 <mattchoque115@gmail.com>
2026-05-31 18:50:29 -0700

    The system for the current player's turn and commands
    I have set up the custom exceptions into the game, and also started to
    set up the another part of the system where the game officially begins
    after checking the conditions to start one.

--- DiceBattleRoyaleProject/src/Game.java
+++ DiceBattleRoyaleProject/src/Game.java
@@ -12,20 +12,27 @@ public class Game {
 		// Setting the players
 		while (input != "quit" || maxSize == playerList.size() || input != "start") {
 			System.out.println("Enter player name");
-			String playerName = sc.nextLine();
-			boolean isAName = GameHelper.checkNameForNumber(playerName); // Check if name isn't just a number
+			String inputLine = sc.nextLine();
+			boolean isAName = GameHelper.checkNameForNumber(inputLine); // Check if name isn't just a number
 			if(!isAName) {
-				System.out.println("This isn't a valid name, try again.");
+				throw new PlayerException("This isn't a valid name, try again.");
+			} else if (inputLine == "quit") {
+				return; // Will exit the game
+			} else if (inputLine == "start"){ // Will start the game
+				if (playerList.size() <= 1) { // If the game tries to start with one or zero players
+					throw new GameException("Game must have two or more players to start.");
+					inputLine = "";
+				}
 			} else {
 				Map<String, PlayerStats> player = new HashMap<>();
-				boolean checkDuplicates = GameHelper.scanForDuplicates(playerList, playerName);
+				boolean checkDuplicates = GameHelper.scanForDuplicates(playerList, inputLine);
 				if(!checkDuplicates) {
-					player.put(playerName, new PlayerStats()); // Creates player
+					player.put(inputLine, new PlayerStats()); // Creates player
 					playerList.add(player); // Insert the player into the list
 				} else {
-					System.out.println("Player name is already on the list, try again.");
+					throw new PlayerException("Player name is already on the list, try again.");
 				}
-			}
+			} 
 		}
 		// Game Conditions on starting
 		boolean startGame = true;
@@ -38,9 +45,30 @@ public class Game {
 			System.out.println("See you around, come back if you have time!");
 		} else {
 			// The game will officially begin.
-			while(playerList.size() == 1) {
+			while(playerList.size() != 1) {
 				Map<String, PlayerStats> player = playerList.remove(); // Current Player
+				// TODO: Player should be able have a random chance to trigger event
 				
+				boolean playerTurn = true;
+				// The player's turn
+				while (playerTurn) {
+					GameHelper.playerMenu(player.getKey());
+					String command = sc.nextLine();
+					// Player will attack another player.
+					if (command.equalsIgnoreCase("attack")) { 
+						
+					} 
+					// Player will have a chance to find item.
+					else if (command.equalsIgnoreCase("search")) {
+						
+					} 
+					// Player will blocking this turn. Reducing any incoming damage by half.
+					else if (command.equalsIgnoreCase("block")) {
+						
+					} else { // If the command is not of any of the three.
+						throw new GameException("Not a valid command, try again.");
+					}
+				}
 			}
 		}
 	}
