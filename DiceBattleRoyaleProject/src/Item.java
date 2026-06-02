0f0d2c7d269b32d4eb0cb0ebe10a4d17e1e8c7cc
MattColaProgrammer93 <mattchoque115@gmail.com>
2026-06-01 22:22:23 -0700

    The Item class added by Gari
    The item class, constructor, and methods are done by Gari. The get
    methods fetch their corresponding private values. useItem will be used
    by the player to gain a boost based on the stat the item will give.

---
+++ DiceBattleRoyaleProject/src/Item.java
@@ -0,0 +1,59 @@
+
+//Gari Garcia
+public class Item {
+    private String name;
+    private String statAffected;
+    private int boostAmount;
+    private boolean used;
+
+    public Item(String name, String statAffected, int boostAmount) {
+        this.name = name;
+        this.statAffected = statAffected;
+        this.boostAmount = boostAmount;
+        this.used = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStatAffected() {
+        return statAffected;
+    }
+
+    public int getBoostAmount() {
+        return boostAmount;
+    }
+
+    public boolean isUsed() {
+        return used;
+    }
+
+    public void useItem(Map<String, Object> player) {
+        if (used) {
+            throw new IllegalStateException("This item was already used.");
+        }
+
+        if (statAffected.equalsIgnoreCase("health")) {
+            int health = (int) player.get("health");
+            player.put("health", health + boostAmount);
+        } 
+        else if (statAffected.equalsIgnoreCase("attack")) {
+            int attack = (int) player.get("attack");
+            player.put("attack", attack + boostAmount);
+        } 
+        else if (statAffected.equalsIgnoreCase("defense")) {
+            int defense = (int) player.get("defense");
+            player.put("defense", defense + boostAmount);
+        } 
+        else {
+            throw new IllegalArgumentException("Invalid stat: " + statAffected);
+        }
+
+        used = true;
+    }
+
+    public String toString() {
+        return name + " boosts " + statAffected + " by " + boostAmount;
+    }
+}
