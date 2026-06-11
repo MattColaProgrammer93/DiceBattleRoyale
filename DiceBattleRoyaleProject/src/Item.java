f0d2852995f1074115b50b82388f90ae6662262e
MattColaProgrammer93 <mattchoque115@gmail.com>
2026-06-10 18:15:33 -0700

    Revamped item class by Gari
    The item class has been revamped by a teammate of mine to increase the
    basic stats of a player when they used a item. I also went ahead to add
    a blank constructor to be able to insert item class object into map.

--- DiceBattleRoyaleProject/src/Item.java
+++ DiceBattleRoyaleProject/src/Item.java
@@ -1,13 +1,22 @@
+// Gari Garcia
 import java.util.Map;
 
-//Gari Garcia
 public class Item {
     private String name;
     private String statAffected;
-    private int boostAmount;
+    private double boostAmount;
     private boolean used;
-
-    public Item(String name, String statAffected, int boostAmount) {
+    
+    // Blank Item
+    public Item() {
+    	name = "null";
+    	statAffected = "null";
+    	boostAmount = 0.0;
+    	used = false;
+    }
+    
+    // Current Item
+    public Item(String name, String statAffected, double boostAmount) {
         this.name = name;
         this.statAffected = statAffected;
         this.boostAmount = boostAmount;
@@ -22,7 +31,7 @@ public class Item {
         return statAffected;
     }
 
-    public int getBoostAmount() {
+    public double getBoostAmount() {
         return boostAmount;
     }
 
@@ -36,16 +45,24 @@ public class Item {
         }
 
         if (statAffected.equalsIgnoreCase("health")) {
-            int health = (int) player.get("health");
-            player.put("health", health + boostAmount);
-        } 
-        else if (statAffected.equalsIgnoreCase("attack")) {
-            int attack = (int) player.get("attack");
-            player.put("attack", attack + boostAmount);
+            int health = (int) player.getOrDefault("health", 100);
+            player.put("health", health + (int) boostAmount);
         } 
         else if (statAffected.equalsIgnoreCase("defense")) {
-            int defense = (int) player.get("defense");
-            player.put("defense", defense + boostAmount);
+            int defense = (int) player.getOrDefault("defense", 10);
+            player.put("defense", defense + (int) boostAmount);
+        } 
+        else if (statAffected.equalsIgnoreCase("baseDamage")) {
+            int baseDamage = (int) player.getOrDefault("baseDamage", 12);
+            player.put("baseDamage", baseDamage + (int) boostAmount);
+        } 
+        else if (statAffected.equalsIgnoreCase("critChance")) {
+            double critChance = (double) player.getOrDefault("critChance", 0.10);
+            player.put("critChance", critChance + boostAmount);
+        } 
+        else if (statAffected.equalsIgnoreCase("critBonus")) {
+            double critBonus = (double) player.getOrDefault("critBonus", 1.5);
+            player.put("critBonus", critBonus + boostAmount);
         } 
         else {
             throw new IllegalArgumentException("Invalid stat: " + statAffected);
